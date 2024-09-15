08c603e78c0ced50e66195117adc00d8f474ce40
Kim-Tri Huynh <huynhkimtri.fuhcm@gmail.com>
2024-09-15 16:05:37 +0700

    feat: Add filter bookshelf and constant

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
 import "./App.css";
 import { useState } from "react";
 import Bookshelf from "./components/Bookshelf";
+import * as BooksAPI from "./utils/BooksAPI";
+import { CURRENTLY_READING, READ, WANT_TO_READ } from "./utils/constants";
 
 function App() {
   const [showSearchPage, setShowSearchpage] = useState(false);
+  const [lstBook, setLstBook] = useState([]);
+
+  const lstBookCurrentRead = lstBook.filter(
+    (book) => CURRENTLY_READING === book.shelf
+  );
+  const lstBookWantToRead = lstBook.filter(
+    (book) => WANT_TO_READ === book.shelf
+  );
+  const lstBookRead = lstBook.filter((book) => READ === book.shelf);
+
+  useState(() => {
+    const getAllListOfBook = async () => {
+      const res = await BooksAPI.getAll();
+      console.log("🚀 ~ getAllListOfBook ~ res:", res);
+      setLstBook(res);
+    };
+
+    getAllListOfBook();
+  }, []);
 
   return (
     <div className="app">
@@ -33,9 +54,9 @@ function App() {
             <h1>MyReads</h1>
           </div>
           <div className="list-books-content">
-            <Bookshelf title="Currently Reading" />
-            <Bookshelf title="Want to Read" />
-            <Bookshelf title="Read" />
+            <Bookshelf title="Currently Reading" lstBook={lstBookCurrentRead} />
+            <Bookshelf title="Want to Read" lstBook={lstBookWantToRead} />
+            <Bookshelf title="Read" lstBook={lstBookRead} />
           </div>
           <div className="open-search">
             <a onClick={() => setShowSearchpage(!showSearchPage)}>Add a book</a>
